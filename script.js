25829b61ea9bde02b34768aac247da32cec735ba
electricSoda <justin.hua.ge@gmail.com>
2021-02-14 11:10:46 -0700

    Chat functionality

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const pinappend = document.getElementById('pinappend');
-new URLSearchParams(window.location.search).forEach((value) => {
-    pinappend.append(document.createElement(`${value}`))
-    pinappend.append(document.createElement('br') 
-,)});
+var valuess = [];
+new URLSearchParams(window.location.search).forEach((name, value) => {
+    valuess.push(name), 
+    console.log(valuess)
+});
+
+console.log(valuess[1])
+
+
+function sendmessage() {
+    const textarea = document.getElementById('text-area-object');
+    var message = document.getElementById('submsg').value;
+    var y = document.createTextNode(valuess[1]);
+    var sub = document.createTextNode(' >    ');
+    var x = document.createTextNode(message);
+    textarea.appendChild(y);
+    textarea.append(sub);
+    textarea.append(x)
+    textarea.append(document.createElement('br'))
+    document.getElementById('submsg').value='';
+}
